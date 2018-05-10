6e1f31599b42eba8c0111edca57de2b1f80db67a
HannaLindwall <dic14hli@student.lu.se>
2018-05-10 15:02:21 +0200

    working on the opTable

--- assignmnt/assignment2/stringAlign.hs
+++ assignmnt/assignment2/stringAlign.hs
@@ -3,8 +3,8 @@ import qualified Data.List as List
 
 type AlignmentType = (String,String)
 
-outputOptAlignments string1 string2 = putStrLn ("There are " ++ show (length xs) ++ " optimal alignments:\n\n" ++ (buildString xs) ++ "There are " ++ show (length xs) ++ " optimal alignments!")
-    where xs = optAlignments string1 string2
+outputOptAlignments string1 string2 = putStrLn ("There are " ++ show (scr) ++ " optimal alignments:\n\n" ++ (buildString xs) ++ "There are " ++ show (scr) ++ " optimal alignments!")
+    where (scr, xs) = optAlignments string1 string2
 
 buildString [] = []
 buildString (x:xs) = ((fst x) ++ "\n" ++ (snd x) ++ "\n\n") ++ (buildString xs)
@@ -19,36 +19,38 @@ optAlignments (s1:string1) (s2:string2) = maximaBy currScore (concat [attachHead
     , attachHeads s1 '-' (optAlignments string1 (s2:string2)) ])
 ¦ -}
 
-optAlignments :: String -> String -> [AlignmentType]
-optAlignments [] [] = [([], [])]
-optAlignments (x:xs) [] = attachHeads x '-' (optAlignments xs [])
-optAlignments [] (y:ys) = attachHeads '-' y (optAlignments [] ys)
-optAlignments (s1:string1) (s2:string2) = maximaBy currScore (concat [attachHeads s1 s2 (optAlignments string1 string2)
-    , attachHeads '-' s2 (optAlignments (s1:string1) string2)
-    , attachHeads s1 '-' (optAlignments string1 (s2:string2)) ])
+optAlignments' :: String -> String -> [AlignmentType]
+optAlignments' [] [] = [([],[])]
+optAlignments' (x:xs) [] = attachHeads x '-' (optAlignments' xs [])
+optAlignments' [] (y:ys) = attachHeads '-' y (optAlignments' [] ys)
+optAlignments' (s1:string1) (s2:string2) = maximaBy currScore (concat [attachHeads s1 s2 (optAlignments' string1 string2)
+    , attachHeads '-' s2 (optAlignments' (s1:string1) string2)
+    , attachHeads s1 '-' (optAlignments' string1 (s2:string2)) ])
 
-
-
-
-alignLength :: Eq a => [a] -> [a] -> [AlignmentType]
-alignLength xs ys = alignLen (length xs) (length ys)
+optAlignments :: String -> String -> (Int, [AlignmentType])
+optAlignments xs ys = alignLen (length xs) (length ys)
   where
     alignLen i j = alignTable!!i!!j
     alignTable = [[ alignEntry i j | j<-[0..]] | i<-[0..] ]
 
-    alignEntry :: Int -> Int -> AlignmentType
-    alignEntry _ 0 = 0
-    alignEntry 0 _ = 0
-    alignEntry i j
-
-
-      maximaBy currScore ()
-      | x == y    = 1 + alignLen (i-1) (j-1)
-      | otherwise = max (alignLen i (j-1))
-                        (alignLen (i-1) j)
+    alignEntry :: Int -> Int -> (Int, [AlignmentType])
+    alignEntry 0 0 = (0,[])
+    alignEntry i 0 = ((-1), [attachTails x '-' (alignLen (i-1) 0)])
       where
-         x = xs!!(i-1)
+        x = xs!!(i-1)
+    alignEntry 0 j = ((-1), [attachTails '-' y (alignLen 0 (j-1))])
+      where
          y = ys!!(j-1)
+    alignEntry i j = maximaBy fst [(scr1 , [algns1]), ( scr2 , [algns2]), ( scr3, [algns3])]
+      where
+        scr1 = fst alignLen (i-1) (j-1) + score (x,y)
+        scr2 = fst alignLen i (j-1) + score ('-',y)
+        scr3 = fst alignLen (i-1) j + score (x,'-')
+        algns1 = attachTails x y snd (alignLen (i-1) (j-1))
+        algns2 = attachTails '-' y snd (alignLen i (j-1))
+        algns3 = attachTails x '-' snd (alignLen (i-1) j)
+        x = xs!!(i-1)
+        y = ys!!(j-1)
 
 -- func är funktionen som sorterar på högsta scoren, tar in ett par
 currScore :: (String, String) -> Int
@@ -79,33 +81,33 @@ score' (x, '-') = (-1)
 score' ('-', y) = (-1)
 score' (x,y) = if x == y then 0 else (-1)
 
- similarityScore :: String -> String -> Int
- similarityScore [] [] = 0
- similarityScore _ [] = (-1)
- similarityScore [] _ = (-1)
- similarityScore xs ys = simLen   (length xs) (length ys)
- where
-   simLen i j = simTable!!i!!j
-   simTable = [[ simEntry i j | j<-[0..]] | i<-[0..] ]
+similarityScore :: String -> String -> Int
+similarityScore [] [] = 0
+similarityScore _ [] = (-1)
+similarityScore [] _ = (-1)
+similarityScore xs ys = simLen (length xs) (length ys)
+  where
+    simLen i j = simTable!!i!!j
+    simTable = [[ simEntry i j | j<-[0..]] | i<-[0..] ]
 
-   simEntry :: Int -> Int -> Int
-   simEntry _ 0 = (-1)
-   simEntry 0 _ = (-1)
-   simEntry i j =
-     max (simLen (i-1) (j-1) + score (x,y))
-     (max (simLen i (j-1) + score(x,'-')) (simLen (i-1) j + score('-',y)))
-     where
-       x = xs!!(i-1)
-       y = ys!!(j-1)
+    simEntry :: Int -> Int -> Int
+    simEntry _ 0 = (-1)
+    simEntry 0 _ = (-1)
+    simEntry i j = max ((simLen (i-1) (j-1)) + score (x,y)) (max ((simLen i (j-1)) + score(x,'-')) ((simLen (i-1) j) + score('-',y)))
+      where
+        x = xs!!(i-1)
+        y = ys!!(j-1)
 
-
-       score (x, '-') = (-1)
-       score ('-', y) = (-1)
-       score (x,y) = if x == y then 0 else (-1)
+score (x, '-') = (-1)
+score ('-', y) = (-1)
+score (x,y) = if x == y then 0 else (-1)
 
 -- h1 and h2 gets attached as heads to the lists inside the every tuple in aList
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
 attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
+
+attachTails :: a -> a -> [([a],[a])] -> [([a],[a])]
+attachTails h1 h2 aList = [(xs ++ [h1], ys ++ [h2]) | (xs,ys) <- aList]
 
 sort func xs = List.sortBy (\x y -> func x `compare` func y) xs
 
